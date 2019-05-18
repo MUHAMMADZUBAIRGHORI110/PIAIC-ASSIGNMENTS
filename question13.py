ac6f13b74b9ebe317b9c5cb96675f8cf882a5bd1
MUHAMMADZUBAIRGHORI110 <50289506+MUHAMMADZUBAIRGHORI110@users.noreply.github.com>
2019-05-18 22:51:53 +0500

    Delete question13.py

--- question13.py
+++
@@ -1,6 +0,0 @@
-value1 = int(input("Enter First Value: "))
-value2 = int(input("Enter Second Value: "))
-if value1+value2 == 5 or abs(value1-value2) == 5 or value1==value2:
-    print ("True")
-else:
-    print ("False")
