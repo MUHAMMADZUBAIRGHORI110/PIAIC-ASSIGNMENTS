b61dafc22a1f3e99cfb5953ce3e6ae0cd53af3bb
MUHAMMADZUBAIRGHORI110 <50289506+MUHAMMADZUBAIRGHORI110@users.noreply.github.com>
2019-05-18 22:52:34 +0500

    Delete question2.py

--- question2.py
+++
@@ -1,7 +0,0 @@
-number = int(input("Enter the number:   "))
-if number == 0:
-    print ("Your number is zero")
-elif number>0:
-    print ("Your number is positive")
-else:
-    print ("Your number is negative")
