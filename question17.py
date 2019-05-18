62c3452e1d330186291445ecc6d8564a47b3320d
MUHAMMADZUBAIRGHORI110 <50289506+MUHAMMADZUBAIRGHORI110@users.noreply.github.com>
2019-05-18 22:52:20 +0500

    Delete question17.py

--- question17.py
+++
@@ -1,6 +0,0 @@
-hight = input("Enter Height in inches ")
-feet = input("Enter Height in fee  ")
-hight1 = float(hight)*2.54
-feet1 = float(feet)*30.48
-cm = hight1+feet1
-print ("your height is" , cm ,'cm')
