3e95598462fdef9675f34a7d9b259fef3e405925
MUHAMMADZUBAIRGHORI110 <50289506+MUHAMMADZUBAIRGHORI110@users.noreply.github.com>
2019-05-18 22:52:10 +0500

    Delete question15.py

--- question15.py
+++
@@ -1,5 +0,0 @@
-principle = float(input("Enter Principle Amount: "))
-interest = float(input("Enter Interest rate "))
-years = float(input("Enter Number of years"))
-calc = principle*(1+(0.01*interest))**years
-print (calc)
